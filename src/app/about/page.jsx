37c517da54c06384d954e2e6ed22a07d1ec8d08d
marksikaundi <emmanuelmark117@gmail.com>
2023-06-24 04:41:38 -0700

    commit updates

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import styles from "./page.module.css";
+
 
 const About = () => {
   return (
--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <div className="container">
         <Navbar />
+        </div>
 
         {children}
         <Footer />
